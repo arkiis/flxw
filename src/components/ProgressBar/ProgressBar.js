5a5e97fe45cbcc08db4d860b206f2a962edbf12a
Your Name <william.fleckenstein@gmail.com>
2019-10-30 21:30:12 -0500

    added console.log

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,6 +3,8 @@ import * as Styles from "./ProgressBar.styles";
 import PropTypes from "prop-types";
 
 const ProgressBar = props => {
+  console.log("I am adding stuff to refactor branch");
+  console.log("hello world");
   return (
     //These are the progress bars for the dashboard page
     //under Today's market activity
