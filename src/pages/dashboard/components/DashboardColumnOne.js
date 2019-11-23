a3a4b8aee8dea1b6baaf82a16cd3436cd43fa8f6
Your Name <william.fleckenstein@gmail.com>
2019-11-22 18:07:58 -0600

    DashboardfavoriteItem list moved into its own component

--- src/pages/dashboard/components/DashboardColumnOne.js
+++ src/pages/dashboard/components/DashboardColumnOne.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import * as Home from "../dashboard.styles";
-import * as Styles from "../../../components/Table/Tables.styles";
+
 import firebase from "../../../Firebase/Firebase";
-import DashLineChart from "./DashLineChart";
+import DashboardFavoriteItems from "./DashboardFavoriteItems";
 import { Scrollbars } from "react-custom-scrollbars";
 import FollowIcon from "../../../assets/images/no-coins-01.svg";
 import DashboardMarketAct from "./DashboardMarketAct";
@@ -32,7 +32,7 @@ const DashboardColumnOne = props => {
 
   const [favoriteCoin, setFavoriteCoin] = useState([]);
   const [percentage, setPercentage] = useState(0);
-  console.log(favoriteCoin);
+
   return (
     <Home.DashboardFollowing>
       <Home.FollowingSection>
@@ -47,38 +47,11 @@ const DashboardColumnOne = props => {
               ></img>
             ) : (
               //Mapping the coins into the following component on the dashboard
-              <Home.DashboardFavoriteItems>
-                {favoriteCoin.map((x, index) => {
-                  return (
-                    <Home.FavoriteCoin key={index}>
-                      <Home.LogoAndNameWrapper>
-                        <Styles.CoinIcon src={x.data().logo_url} marginR />
-                        <Styles.TableDataP>{x.data().name}</Styles.TableDataP>
-                      </Home.LogoAndNameWrapper>
-
-                      <div style={{ display: "flex", width: "200px" }}>
-                        <Home.FavoriteCoinPrice>
-                          ${props.simplifyPrice(x.data().price)}
-                        </Home.FavoriteCoinPrice>
-                      </div>
-
-                      {/* Line chart  */}
-                      <DashLineChart />
-                      {/* deleting data */}
-                      {/* if user clicks this element,
-                      that coin will be del */}
-                      <Home.DeleteFollowing
-                        onClick={e => {
-                          e.stopPropagation();
-                          db.collection("coins")
-                            .doc(x.id)
-                            .delete();
-                        }}
-                      ></Home.DeleteFollowing>
-                    </Home.FavoriteCoin>
-                  );
-                })}
-              </Home.DashboardFavoriteItems>
+              <DashboardFavoriteItems
+                favoriteCoin={favoriteCoin}
+                db={db}
+                simplifyPrice={props.simplifyPrice}
+              />
             )}
           </Scrollbars>
         </Home.DashboardPortfolioSection>
